b03a9ef3d5a44f7665152f50162f3dda050088d9
stefano marra <stefano.marra@cgi.com>
2018-09-20 11:43:45 +0200

    feat(map-react-mobx): add map component

---
+++ packages/map-react-mobx/src/map/map-component.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import { MapRendererController } from '@cgi-eo/map-mobx';
+
+export interface MapComponentProps {
+    rendererController: MapRendererController;
+}
+
+export class MapComponent extends React.Component<MapComponentProps> {
+
+    private mapContainer_: HTMLElement;
+
+    constructor(props: MapComponentProps) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.rendererController.setDomTarget(this.mapContainer_);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentWillUnmount() {
+        this.props.rendererController.setDomTarget(null);
+    }
+
+    render() {
+        return (
+            <div
+                className='map-widget'
+                style={{ width: '100%', height: '100%' }}
+                ref={(element) => this.mapContainer_ = element}
+            ></div>
+        );
+    }
+}
