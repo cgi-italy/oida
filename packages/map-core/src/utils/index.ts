26e1cff9908578ef6bdbdddf8f6ede09150a4a34
stefano marra <stefano.marra@cgi.com>
2018-09-20 11:25:44 +0200

    feat(map-core): add subscription tracker helper class

--- packages/map-core/src/utils/index.ts
+++ packages/map-core/src/utils/index.ts
@@ -1,2 +1,2 @@
 export * from './dynamic-factory';
-
+export * from './subscription-tracker';
---
+++ packages/map-core/src/utils/subscription-tracker.ts
@@ -0,0 +1,16 @@
+export class SubscriptionTracker {
+    private unsubscribeCbs_: Array<() => any> = [];
+
+    public addSubscription(unsubscribeCb: () => any) {
+      this.unsubscribeCbs_.push(unsubscribeCb);
+    }
+
+    public unsubscribe() {
+        this.unsubscribeCbs_.forEach((unsubscribeCb) => {
+            unsubscribeCb();
+        });
+
+        this.unsubscribeCbs_ = [];
+    }
+
+}
