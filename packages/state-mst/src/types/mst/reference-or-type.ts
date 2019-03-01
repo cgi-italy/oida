b65aa90da890348f5f0be021cd827516b5812b18
stefano marra <stefano.marra@cgi.com>
2019-03-01 11:35:32 +0100

    refactor: use standard reference by default in ReferenceOrType

--- packages/state-mst/src/types/mst/reference-or-type.ts
+++ packages/state-mst/src/types/mst/reference-or-type.ts
@@ -3,7 +3,7 @@ import { types, IAnyModelType, IReferenceType, IMaybe } from 'mobx-state-tree';
 export const ReferenceOrType = <T extends IAnyModelType>(Type: T, referenceType?: IMaybe<IReferenceType<T>>) => {
 
     if (!referenceType) {
-        referenceType =  types.safeReference(Type);
+        referenceType =  types.maybe(types.reference(Type));
     }
     return types.union(
         {
