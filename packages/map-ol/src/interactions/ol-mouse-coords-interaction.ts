4d5d23947de5c8d7c3b01bd03bda59132e4598e9
stefano marra <stefano.marra@cgi.com>
2020-03-18 18:36:58 +0100

    fix: fix for mouse coord interaction not working after ol upgrade

--- packages/map-ol/src/interactions/ol-mouse-coords-interaction.ts
+++ packages/map-ol/src/interactions/ol-mouse-coords-interaction.ts
@@ -1,5 +1,5 @@
 import { listen, unlistenByKey } from 'ol/events';
-import EventType from 'ol/events/EventType';
+import EventType from 'ol/pointer/EventType';
 import { transform } from 'ol/proj';
 
 import { IMouseCoordsInteraction, IMouseCoordsInteractionProps, MOUSE_COORDS_INTERACTION_ID } from '@oida/core';
@@ -38,7 +38,7 @@ export class OLMouseCoordsInteraction implements IMouseCoordsInteraction {
 
         let viewport = this.viewer_.getViewport();
 
-        this.evtSubscriptions_.push(listen(viewport, EventType.MOUSEMOVE, (evt) => {
+        this.evtSubscriptions_.push(listen(viewport, EventType.POINTERMOVE, (evt) => {
             let px = this.viewer_.getEventPixel(evt);
             let coord = this.viewer_.getCoordinateFromPixel(px);
 
@@ -55,7 +55,7 @@ export class OLMouseCoordsInteraction implements IMouseCoordsInteraction {
             }
         }));
 
-        this.evtSubscriptions_.push(listen(viewport, EventType.MOUSEOUT, () => {
+        this.evtSubscriptions_.push(listen(viewport, EventType.POINTEROUT, () => {
             onMouseCoords(null);
         }));
 
