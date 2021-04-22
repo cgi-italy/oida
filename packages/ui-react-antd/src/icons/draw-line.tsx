2bbd13866c930a63263edddda15484c72de0e4a4
stefano marra <stefano.marra@cgi.com>
2021-04-22 15:50:41 +0200

    fix: allow css coloring of draw-line icon

--- packages/ui-react-antd/src/icons/draw-line.tsx
+++ packages/ui-react-antd/src/icons/draw-line.tsx
@@ -9,38 +9,7 @@ export const SvgDrawLine = (props: React.SVGProps<SVGSVGElement>) => (
         fill='currentColor'
         {...props}
     >
-        <defs>
-            <marker
-                orient='auto'
-                refY={0}
-                refX={0}
-                id='draw-line_svg__a'
-                overflow='visible'
-            >
-                <path
-                    d='M.98 0a1 1 0 11-2 0 1 1 0 012 0z'
-                    fillRule='evenodd'
-                    stroke='#000'
-                    strokeWidth={0.2666}
-                />
-            </marker>
-        </defs>
-        <path
-            d='M129.776 661.677l345.16-300.333 421.363 165.855'
-            fill='none'
-            stroke='#000'
-            strokeWidth={62.96}
-            strokeDasharray='0,692.56038615'
-            markerStart='url(#draw-line_svg__a)'
-            markerMid='url(#draw-line_svg__a)'
-            markerEnd='url(#draw-line_svg__a)'
-        />
-        <path
-            d='M132.118 666.807l345.16-300.333L898.64 532.33'
-            fill='none'
-            stroke='#000'
-            strokeWidth={46.59}
-        />
+        <path d='M104.875 272.812a67.288 67.288 0 00-66.937 67.313 67.288 67.288 0 0067.25 67.25 67.288 67.288 0 0014-1.5L300.562 705.75a67.288 67.288 0 00-15.5 43A67.288 67.288 0 00352.375 816a67.288 67.288 0 0067.25-67.25 67.288 67.288 0 00-.25-5.562l414.75-183.5a67.288 67.288 0 0049.188 21.437 67.288 67.288 0 0067.312-67.312 67.288 67.288 0 00-67.312-67.25A67.288 67.288 0 00816 513.813a67.288 67.288 0 00.25 5.5l-414.688 183.5a67.288 67.288 0 00-49.187-21.375 67.288 67.288 0 00-14 1.5L157 383.062a67.288 67.288 0 0015.5-42.937 67.288 67.288 0 00-67.312-67.313 67.288 67.288 0 00-.313 0z' />
     </svg>
 );
 export const DrawLineIcon = (props: any) => (
