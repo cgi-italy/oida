8c9a705b6cc56c62d88ecfe099b0477c54b4882f
stefano marra <stefano.marra@cgi.com>
2022-01-26 12:20:21 +0100

    feat: add utility to get the overall time extent of a set of datasets

--- packages/eo-mobx/src/utils/index.ts
+++ packages/eo-mobx/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './get-dataset-variable-domain';
 export * from './get-raster-band-mode-from-config';
 export * from './get-nearest-dataset-product';
+export * from './get-datasets-time-extent';
 export * from './get-vector-features-filterer.ts';
 export * from './compute-raster-dataset-optimal-band-range';
