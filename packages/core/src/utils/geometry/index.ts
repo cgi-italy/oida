35199f94d59348dd5fdd05e5b4b59bd12f357b07
stefano marra <stefano.marra@cgi.com>
2022-02-21 11:46:37 +0100

    feat: add util to convert a geometry to a wkt string

--- packages/core/src/utils/geometry/index.ts
+++ packages/core/src/utils/geometry/index.ts
@@ -3,3 +3,4 @@ export * from './get-geometry-center';
 export * from './is-valid-extent';
 export * from './flip-geometry-coords';
 export * from './center-on-map';
+export * from './get-geometry-as-wkt';
