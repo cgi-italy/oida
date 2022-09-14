b59a1db531c6f827715173c12ebe29a3d41772f6
stefano marra <stefano.marra@cgi.com>
2022-09-14 12:13:11 +0200

    fix: use uppercase param names in OL WMS source initialization

--- packages/map-ol/src/layers/tilesources/ol-wms-source.ts
+++ packages/map-ol/src/layers/tilesources/ol-wms-source.ts
@@ -5,11 +5,21 @@ import { olTileSourcesFactory } from './ol-tilesources-factory';
 import { getTileGridFromConfig, getUrlFromConfig } from './ol-tilesource-utils';
 
 olTileSourcesFactory.register('wms', function (config) {
+    const parameters = {};
+    // convert all parameters names to uppercase so that
+    // openlayers defaults (e.g. STYLES and FORMAT) are correctly
+    // overriden
+    if (config.parameters) {
+        for (const key in config.parameters) {
+            parameters[key.toUpperCase()] = config.parameters[key];
+        }
+    }
+
     return new TileWMS({
         ...getUrlFromConfig(config),
         params: {
             LAYERS: config.layers,
-            ...config.parameters
+            ...parameters
         },
         crossOrigin: config.crossOrigin,
         tileGrid: config.tileGrid ? getTileGridFromConfig(config.srs, config.tileGrid) : null,
